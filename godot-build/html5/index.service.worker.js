2f0ed6c052d401eeaa991721a078c341bd82a5e0
kaiharuto <kaiharuto.dev@gmail.com>
2023-10-18 18:00:13 +0200

    PitchBuild

--- godot-build/html5/index.service.worker.js
+++ godot-build/html5/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1696814959|17125646401";
+const CACHE_VERSION = "1697644721|93609152941";
 const CACHE_PREFIX = "CakeInvaders3_5-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
