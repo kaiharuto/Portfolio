7a409cda90c62e712affed1e55135b3100ad34ee
kaiharuto <kaiharuto.dev@gmail.com>
2023-10-23 16:02:13 +0200

    Final Version (?)

--- godot-build/html5/index.service.worker.js
+++ godot-build/html5/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1697644721|93609152941";
+const CACHE_VERSION = "1698065596|23302392518";
 const CACHE_PREFIX = "CakeInvaders3_5-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
